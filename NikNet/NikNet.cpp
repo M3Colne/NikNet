4f9b136e3f6fc052223f16ec88d047e372961fac
Nikolas Gurau <ariciultepos2003@yahoo.com>
2021-01-27 16:02:21 +0200

    This is useful for debuging

--- NikNet/NikNet.cpp
+++ NikNet/NikNet.cpp
@@ -5,13 +5,14 @@ using namespace std;
 
 int main()
 {
-    NikNet::Server myServer("127.0.0.1", 54000, false);
+    NikNet::Server myServer("127.0.0.1", 54000, true);
 
     cout << myServer.GetErr();
     
     while (myServer.GetErr() == "No error")
     {
         myServer.Running();
+        cout << "Finished networking ";
     }
 
     cout << myServer.GetErr();
